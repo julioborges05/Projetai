88d2b68ecbeaf9d99a6f74c67fcd55a482e7ac5e
Júlio Borges <julioborges05@hotmail.com>
2024-10-07 19:35:31 -0300

    Inserts initial files to project

---
+++ src/main/java/com/projetai/Projetai/ProjetaiApplication.java
@@ -0,0 +1,13 @@
+package com.projetai.Projetai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetai/Projetai/ProjetaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetai.Projetai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
