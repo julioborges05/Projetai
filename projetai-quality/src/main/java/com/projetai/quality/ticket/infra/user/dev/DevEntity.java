b4a7f298793735c59e14043e8fb64dedef1778a8
Denner Matheus <dennermat@gmail.com>
2024-10-21 17:56:13 -0300

    Added end-point to get a page of tickets

---
+++ projetai-quality/src/main/java/com/projetai/quality/ticket/infra/user/dev/DevEntity.java
@@ -0,0 +1,9 @@
+package com.projetai.quality.ticket.infra.user.dev;
+
+
+import com.projetai.quality.ticket.infra.user.UserEntity;
+
+public class DevEntity extends UserEntity {
+    String id;
+    String name;
+}
