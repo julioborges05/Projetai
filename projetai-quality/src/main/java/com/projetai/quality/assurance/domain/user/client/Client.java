b88d29f0a77e7770cb0bf22f8b109c7096fccf5f
rodrigodiasflamia <rodrigo.flamia@unoesc.edu.br>
2024-10-28 22:06:36 -0300

    Adiciona usuários na Camada de Domínio

---
+++ projetai-quality/src/main/java/com/projetai/quality/assurance/domain/user/client/Client.java
@@ -0,0 +1,9 @@
+package com.projetai.quality.assurance.domain.user.client;
+
+import com.projetai.quality.assurance.domain.user.User;
+
+public class Client extends User {
+    public Client(String name, String email) {
+        super(name, email);
+    }
+}
---
+++ projetai-quality/src/main/java/com/projetai/quality/assurance/domain/user/developer/Developer.java
@@ -0,0 +1,9 @@
+package com.projetai.quality.assurance.domain.user.developer;
+
+import com.projetai.quality.assurance.domain.user.User;
+
+public class Developer extends User {
+    public Developer(String name, String email) {
+        super(name, email);
+    }
+}
